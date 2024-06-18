902719d07759b114b69d81fd0ed0b75e49e91c09
Matheus Leal <matheussleal98@gmail.com>
2024-06-18 16:01:29 -0300

    chore: remove import and method

--- src/main/java/com/barbearia/losbrabos/controllers/ResetPasswordController.java
+++ src/main/java/com/barbearia/losbrabos/controllers/ResetPasswordController.java
@@ -3,7 +3,6 @@ package com.barbearia.losbrabos.controllers;
 import com.barbearia.losbrabos.domain.user.ResetPasswordRequestDTO;
 import com.barbearia.losbrabos.services.impl.ResetPasswordServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/com/barbearia/losbrabos/controllers/SessionsController.java
+++ src/main/java/com/barbearia/losbrabos/controllers/SessionsController.java
@@ -1,10 +1,8 @@
 package com.barbearia.losbrabos.controllers;
 
 import com.barbearia.losbrabos.domain.user.AuthenticationDTO;
-import com.barbearia.losbrabos.domain.user.CreateUserDTO;
 import com.barbearia.losbrabos.domain.user.LoginResponseDTO;
 import com.barbearia.losbrabos.services.impl.AuthenticateServiceImpl;
-import com.barbearia.losbrabos.services.impl.CreateUserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -14,19 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-public class AuthenticationController {
+public class SessionsController {
     private final AuthenticateServiceImpl authenticateService;
-    private final CreateUserServiceImpl createUserService;
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody @Validated AuthenticationDTO data) {
         LoginResponseDTO loginResponseDTO = this.authenticateService.login(data);
         return ResponseEntity.ok(loginResponseDTO);
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity register(@RequestBody @Validated CreateUserDTO data) {
-        this.createUserService.create(data);
-        return ResponseEntity.ok().build();
     }
 }
