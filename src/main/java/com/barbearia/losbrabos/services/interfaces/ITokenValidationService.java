aa01da221b7ab0b2f95cb7e66ca40be15e9a40bc
Matheus Leal <matheussleal98@gmail.com>
2024-06-18 14:36:19 -0300

    fix: remove token service class

--- src/main/java/com/barbearia/losbrabos/services/interfaces/ITokenValidationService.java
+++
@@ -1,7 +0,0 @@
-package com.barbearia.losbrabos.services.interfaces;
-
-import com.barbearia.losbrabos.exceptions.InvalidTokenException;
-
-public interface ITokenValidationService {
-    String validateToken(String token) throws InvalidTokenException;
-}
