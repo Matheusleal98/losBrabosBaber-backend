477020e2d4583b6b9da78b1f8da80483e329fb4a
Matheus Leal <matheussleal98@gmail.com>
2024-06-18 14:09:40 -0300

    fix: remove interface IProfileService.

--- src/main/java/com/barbearia/losbrabos/services/interfaces/IProfileService.java
+++
@@ -1,8 +0,0 @@
-package com.barbearia.losbrabos.services.interfaces;
-
-import com.barbearia.losbrabos.domain.user.ProfileDTO;
-import com.barbearia.losbrabos.domain.user.ProfileResponseDTO;
-
-public interface IProfileService {
-    ProfileResponseDTO update(ProfileDTO data);
-}
