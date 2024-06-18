abe3dbaa7e3b0797a91961f8be7ce6868a268ea8
Matheus Leal <matheussleal98@gmail.com>
2024-06-18 16:00:47 -0300

    chore: add method in class

--- src/main/java/com/barbearia/losbrabos/controllers/ProfileController.java
+++ src/main/java/com/barbearia/losbrabos/controllers/ProfileController.java
@@ -2,18 +2,25 @@ package com.barbearia.losbrabos.controllers;
 
 import com.barbearia.losbrabos.domain.user.ProfileDTO;
 import com.barbearia.losbrabos.domain.user.ProfileResponseDTO;
+import com.barbearia.losbrabos.domain.user.User;
+import com.barbearia.losbrabos.services.impl.ShowProfileServiceImpl;
 import com.barbearia.losbrabos.services.impl.UpdateProfileServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class ProfileController {
 
     private final UpdateProfileServiceImpl updateProfileService;
+    private final ShowProfileServiceImpl showProfileService;
+
+    @GetMapping()
+    public ResponseEntity<User> show(@RequestParam Long id){
+        User showProfileUser = showProfileService.showProfile(id);
+        return ResponseEntity.ok(showProfileUser);
+    }
 
     @PutMapping("/profile")
     public ResponseEntity<ProfileResponseDTO> update(@RequestBody ProfileDTO data) {
